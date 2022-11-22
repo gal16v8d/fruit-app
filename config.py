2866f9f016d72fe623b776c9b07d4b1f19c6c84b
alexg <alex.galvis.sistemas@gmail.com>
2022-11-21 22:45:53 -0500

    Minor env update

--- config.py
+++ config.py
@@ -10,11 +10,11 @@ class Config(object):
 
 class DevConfig(Config):
     config = dotenv_values('.env')
-    SQLALCHEMY_DATABASE_URI = config['DB_URL']
+    SQLALCHEMY_DATABASE_URI = config['FRUIT_DB_URL']
 
 
 class TestConfig(Config):
     config = dotenv_values('.env.test')
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = config['DB_URL']
+    SQLALCHEMY_DATABASE_URI = config['FRUIT_DB_URL']
